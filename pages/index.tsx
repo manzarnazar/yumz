df3aa71a852b1560f12760fd86c3dcf5a0f15dbe
manzarnazar <manzarnazar@gmail.com>
2024-12-27 17:15:31 +0500

    fixed routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SEO from "components/seo";
 import WelcomeContainer from "containers/welcome/welcome";
 import WelcomeHero from "components/welcomeHero/welcomeHero";
@@ -13,24 +13,34 @@ import { GetStaticProps } from "next";
 import getLanguage from "utils/getLanguage";
 import { getCookie } from "utils/session";
 import pageService from "services/page";
-import { useAuth } from "contexts/auth/auth.context";
 import { useRouter } from "next/router";
 
-type Props = {};
+type Props = { 
+  initialAuthToken: string | null; // Use initialAuthToken for SSR state
+};
 
-export default function Welcome({}: Props) {
-  const { isAuthenticated } = useAuth();
+export default function Welcome({ initialAuthToken }: Props) {
   const { push } = useRouter();
-
-  console.log("is it ? ",isAuthenticated);
   
-  if (isAuthenticated) {
-    if (typeof window !== "undefined") {
-      push("/home");
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    const authToken = initialAuthToken || getCookie("access_token"); // Get token from initial props or client-side cookies
+    if (authToken) {
+      setIsAuthenticated(true); // Set authentication state after initial render
     }
-    
-    return null; // Prevent rendering the welcome page
-  }
+  }, [initialAuthToken]);
+
+  // If the user is authenticated, redirect to the home page
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     push("/home");
+  //   }
+  // }, [isAuthenticated, push]);
+
+  console.log(isAuthenticated);
+  
+
   const { locale } = useLocale();
 
   const { data } = useQuery(["landingPage", locale], () =>
@@ -63,6 +73,8 @@ export default function Welcome({}: Props) {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const queryClient = new QueryClient();
   const locale = getLanguage(getCookie("locale", ctx));
+  const authToken = getCookie("access_token", ctx); // Check cookie on SSR
+  const initialAuthToken = authToken ? authToken : null; // Pass the token to the client-side component
 
   await queryClient.prefetchQuery(["landingPage", locale], () =>
     pageService.getLandingPage()
@@ -71,6 +83,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {
       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+      initialAuthToken, // Pass initial token to client
     },
     revalidate: 3600,
   };
