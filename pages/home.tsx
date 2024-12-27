c43b8c83e1f63e66ce01568878ffc7bcac64e7f3
manzarnazar <manzarnazar@gmail.com>
2024-12-27 14:24:59 +0500

    fixes

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,6 +3,7 @@ import FooterMenu from "containers/footerMenu/footerMenu";
 import { useAppSelector } from "hooks/useRedux";
 import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
+import { selectUserCart } from "redux/slices/userCart";
 
 import informationService from "services/information";
 import createSettings from "utils/createSettings";
@@ -21,6 +22,13 @@ type HomeProps = {
 export default function Home({ uiType = "1" }: HomeProps) {
   const Ui = uiTypes[uiType];
   const Homev1 = uiTypes["1"];
+
+
+  const cart = useAppSelector(selectUserCart);
+    // const cartIndicatorVisible = useAppSelector(state => state.userCart.indicatorVisible)
+    // const cartItems = cart?.user_carts.flatMap((item) => item.cartDetails) || [];
+
+  
 
 
   return (
